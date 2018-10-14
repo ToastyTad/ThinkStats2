56a391a55beae9402ac3406aff73943a462348ab
ToastyTad <toastytadgit@gmail.com>
2018-10-14 17:08:02 -0400

    ch1 and ch2 changes

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -1,17 +1,65 @@
 """This file contains code for use with "Think Stats",
 by Allen B. Downey, available from greenteapress.com
 
-Copyright 2014 Allen B. Downey
+Copyright 2010 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
-from __future__ import print_function
+from __future__ import print_function, division
 
-import numpy as np
 import sys
-
+import numpy as np
 import nsfg
 import thinkstats2
+
+from collections import defaultdict
+
+
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz',
+                nrows=None):
+    """Reads the NSFG respondent data.
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    CleanFemResp(df)
+    return df
+
+
+def CleanFemResp(df):
+    """Recodes variables from the respondent frame.
+
+    df: DataFrame
+    """
+    pass
+
+
+def ValidatePregnum(resp):
+    """Validate pregnum in the respondent file.
+
+    resp: respondent DataFrame
+    preg: pregnancy DataFrame
+    """
+    # read frame and make the map from caseid to list of pregnancy indices
+    preg_map = nsfg.MakePregMap(nsfg.ReadFemPreg())
+    
+    # iterate through the respondent pregnum series
+    for index, pregnum in resp.pregnum.iteritems():
+        caseid = resp.caseid[index]
+        indices = preg_map[caseid]
+
+        # check that pregnum from the respondent file equals
+        # the number of records in the pregnancy file
+        if len(indices) != pregnum:
+            print(caseid, len(indices), pregnum)
+            return False
+
+    return True
 
 
 def main(script):
@@ -19,7 +67,11 @@ def main(script):
 
     script: string script name
     """
-    print('%s: All tests passed.' % script)
+    resp = ReadFemResp()
+    print(len(resp))
+    print(resp.pregnum.value_counts()[1])
+    assert(ValidatePregnum(resp))    
+    print('All tests passed.')
 
 
 if __name__ == '__main__':
